80ec067c91c5c47236a3d837ce991b01a1a4d5bb
Petrus <petrusheya@gmail.com>
2024-08-08 23:40:20 +0200

    Added email extraction functions

--- utils.js
+++ utils.js
@@ -2,40 +2,8 @@ import axios from 'axios'
 import * as cheerio from 'cheerio'
 import url from 'url'
 
-export async function extractWebsiteData(url){
-
-    const emails = []
-    const emailRegex = /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+/g;
-    const aTags = []
-    const socialMedia = []
-    const socialMediaRegexes = {
-        linkedin: /https?:\/\/(www\.)?linkedin\.com\/[a-zA-Z0-9-_/]+/g,
-        instagram: /https?:\/\/(www\.)?instagram\.com\/[a-zA-Z0-9-_/]+/g,
-        facebook: /https?:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9-_/]+/g,
-        twitter: /https?:\/\/(www\.)?twitter\.com\/[a-zA-Z0-9-_/]+/g,
-    };
-
+function getInternalLinks(pageUrl, html) {
     try {
-        const { data: html } = await axios.get(url)
-        const $ = cheerio.load(html);
-
-        const textContent = $('body').text()
-        const matchedEmails = textContent.match(emailRegex);
-        if (matchedEmails) {
-            emails.push(...matchedEmails);
-        }
-
-
-
-
-    } catch (error) {
-        
-    }
-
-}
-async function getInternalLinks(pageUrl) {
-    try {
-        const { data: html = '' } = await axios.get(pageUrl);
         const $ = cheerio.load(html);  
         const baseUrl = getRootDomain(pageUrl)
         const internalLinks = [];
@@ -58,9 +26,44 @@ async function getInternalLinks(pageUrl) {
         return []
     }
 }
-
 function getRootDomain(url) {
     const parsedUrl = new URL(url);
     const domain = parsedUrl.hostname;
     return domain;
+}
+async function getEmails(url){
+    const emails = []
+    
+    try {
+        const initialemails = await getEmailsSinglePage(url)
+        emails.push(...initialemails)
+        const aTags = getInternalLinks(url, html)
+
+        for (const link of aTags){
+            const returnedEmails = await getEmailsSinglePage(link)
+            emails.push(...returnedEmails)
+        }
+
+        return []
+    } catch (error) {
+        return []
+    }
+}
+async function getEmailsSinglePage(url){
+    const emails = []
+    const emailRegex = /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+/g;
+
+    try {
+        const { data: html } = await axios.get(url)
+        const $ = cheerio.load(html);        
+        const textContent = $('body').text()
+        const matchedEmails = textContent.match(emailRegex);
+        if (matchedEmails) {
+            emails.push(...matchedEmails);
+        }
+        return emails
+    } catch (error) {
+        return []
+    }
+
 }
