df4e239fc497c54a2e7354230b964659cb0ae5ef
Petrus <petrusheya@gmail.com>
2024-08-10 02:52:59 +0200

    Added email verification function

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import * as cheerio from 'cheerio'
+import emailValidator from 'deep-email-validator'
 
 async function getEmails(url){
     const emails = []
@@ -7,9 +8,9 @@ async function getEmails(url){
     try {
         const initialemails = await getEmailsSinglePage(url)
         emails.push(...initialemails)
-        const aTags = getInternalLinks(url, html)
+        const links = getInternalLinks(url, html)
 
-        for (const link of aTags){
+        for (const link of links){
             const returnedEmails = await getEmailsSinglePage(link)
             emails.push(...returnedEmails)
         }
@@ -66,3 +67,48 @@ async function getEmailsSinglePage(url){
     }
 
 }
+async function getSocialLinks(url){
+    const twitterRegex = /https?:\/\/(www\.)?twitter\.com\/[A-Za-z0-9_]+/g
+    const facebookRegex = /https?:\/\/(www\.)?facebook\.com\/[A-Za-z0-9.]+/g
+    const linkedinRegex = /https?:\/\/(www\.)?linkedin\.com\/in\/[A-Za-z0-9-]+/g
+    const instagramRegex = /https?:\/\/(www\.)?instagram\.com\/[A-Za-z0-9_.]+/g
+    const youtubeRegex = /https?:\/\/(www\.)?(youtube\.com\/(channel|c|user|watch\?v=)|youtu\.be\/)[A-Za-z0-9_\-]+/g
+
+    try {
+        const { data: html } = await axios.get(url)
+        const $ = cheerio.load(html);        
+        const textContent = $('body').text()
+        const twitterLinks = textContent.match(twitterRegex);
+        const facebookLinks = textContent.match(facebookRegex);
+        const linkedinLinks = textContent.match(linkedinRegex);
+        const instagramLinks = textContent.match(instagramRegex);
+        const youtubeLinks = textContent.match(youtubeRegex)
+
+        return {
+            twitter: twitterLinks,
+            facebook: facebookLinks,
+            linkedin: linkedinLinks,
+            instagram: instagramLinks,
+            youtube: youtubeLinks
+        }
+    } catch (error) {
+        return {}
+    }
+
+}
+async function verifyEmail(email) {
+    emailValidator.validate(email) // make sure to add IP roatation
+}
+async function getPhoneNumber(url) {
+    const phoneRegex = /\+?(\d{1,3})?[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/g;
+    try {
+        const { data: html } = await axios.get(url)
+        const $ = cheerio.load(html);        
+        const textContent = $('body').text()
+        const phoneNumbers = textContent.match(phoneRegex);
+        return phoneNumbers
+    } catch (error) {
+        return []
+    }
+}
+
