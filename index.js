aa98482ad69a3c35c4660024e2e45cb590975cc0
Petrus <petrusheya@gmail.com>
2024-08-21 22:03:11 +0200

    Added network intecept

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ app.post('/api/google-maps', async (req,res)=>{
         browser = await puppeteer.connect({
             browserWSEndpoint: process.env.BROWSER_URL
         })
-        page = await browser.newPage()
+        page = await browser.newPage();  
+        await page.setRequestInterception(true);  
+        page.on('request', (request) => {  
+            if (request.resourceType() === 'image') {  
+                request.abort();  
+            } else {  
+                request.continue();  
+            }
+        })
+          
         await page.goto(`https://www.google.com/localservices/prolist?g2lbs=AIQllVxEpXuuCPFrOHRAavT6nJMeIXUuM9D7r7-IlczaiEuKdgYVA09lqC7MIhZ3mUJ_MfwMM30K5vDmEB9UFLvwoZMUuqe_RIT2RmrDlIhrFndV8WuAgW-ioANkhbKSz__jtHfxKrJZLfFak9ca1Vbqi4HEnaKw7Q%3D%3D&hl=en-US&gl=&cs=1&ssta=1&q=${service}+in+${location}&oq=${service}+in+${location}&scp=Cg5nY2lkOmFyY2hpdGVjdBJMEhIJSTKCCzZwQIYRPN4IGI8c6xYaEgkLNjLkhLXqVBFCt95Dkrk7HCIKVGV4YXMsIFVTQSoUDV1uZg8VcypvwB3BkMEVJTvOQ8gwABoKYXJjaGl0ZWN0cyITYXJjaGl0ZWN0cyBpbiB0ZXhhcyoJQXJjaGl0ZWN0&slp=MgA6HENoTUkxWXZoamNfVmhBTVZZSUJRQmgxMkpBRTRSAggCYACSAZsCCgsvZy8xdGg2ZjZ4ZwoNL2cvMTFoY3c1ZDltZAoLL2cvMXd5YzRybWQKDC9nLzEycWg5dzhmZAoNL2cvMTFnNm5sMGxmNQoLL2cvMXRkY2dzdjQKCy9nLzF0aGwxODBzCgsvZy8xdGc3c2RmNwoLL2cvMXRkNGR6cTEKCy9nLzF0ZnNuZDRfCg0vZy8xMWI3bHBtOGIxCgsvZy8xdHp6dng1bAoLL2cvMXRrNHJsMTEKCy9nLzF0a3ZiNGpzCg0vZy8xMWJ4OGNteHM4Cg0vZy8xMWNuMF93MTkxCgsvZy8xdG15NWdzaAoLL2cvMXYzaF9jM3EKCy9nLzF2eWsyeHpnCgsvZy8xdGZtY24xcRIEEgIIARIECgIIAZoBBgoCFxkQAA%3D%3D&src=2&serdesk=1&sa=X&ved=2ahUKEwiyo9uNz9WEAxUMQkEAHZWwBcEQjGp6BAgfEAE&lci=${(parseInt(pagination))*20}`)
 
         // Wait for cards to load
@@ -55,8 +64,8 @@ app.post('/api/google-maps', async (req,res)=>{
 
     } catch (error) {
         console.error("Error in GMP Scraper:", error.message)
-        await page.close()
-        await browser.close()
+        await page?.close()
+        await browser?.close()
         return res.sendStatus(500)
     }
 })
